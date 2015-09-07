26953fbed138157d8c9cdbdc311b677913d45f75
mzdu <daniel_pub@hotmail.com>
2015-09-07 12:34:05 -0400

    update

--- src/strStr_codingStyle/strStr.py
+++ src/strStr_codingStyle/strStr.py
@@ -5,7 +5,6 @@ class Solution:
     def strStr(self, source, target):
         if source == None or target == None:
             return -1
-        
         if target == '':
             return 0
         
@@ -13,21 +12,17 @@ class Solution:
         
         # 2. searching starts from index 0, for source
         i = 0
-
         
         while i < searchLength:
-            
             # j is the index for target
             j = 0            
             while j < len(target):
                 if source[i + j] != target[j]:
                     break
-                
                 j += 1
             
             if j == len(target):
                 return i
-                
             i += 1
         
         return -1
